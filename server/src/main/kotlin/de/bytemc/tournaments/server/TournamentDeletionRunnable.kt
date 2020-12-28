f5a5efb952125b212758fa05f2ce887d24bead37
Nico_ND1 <nicondlouis@gmail.com>
2020-12-28 16:25:59 +0100

    Fix bug where tournament was instantly deleted

--- server/src/main/kotlin/de/bytemc/tournaments/server/TournamentDeletionRunnable.kt
+++ server/src/main/kotlin/de/bytemc/tournaments/server/TournamentDeletionRunnable.kt
@@ -1,14 +1,18 @@
 package de.bytemc.tournaments.server
 
 import de.bytemc.tournaments.api.TournamentState
+import java.util.concurrent.TimeUnit
 
 /**
  * @author Nico_ND1
  */
 class TournamentDeletionRunnable : Runnable {
+
+    private val timeout = TimeUnit.MINUTES.toMillis(1)
+
     override fun run() {
         for (tournament in ServerTournamentAPI.instance.tournaments(TournamentState.FINISHED)) {
-            if (tournament.endTime != 0L && tournament.endTime < System.currentTimeMillis()) {
+            if (tournament.endTime != 0L && tournament.endTime < System.currentTimeMillis() + timeout) {
                 ServerTournamentAPI.instance.deleteTournament(tournament)
             }
         }
